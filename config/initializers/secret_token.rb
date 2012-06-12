73be8437c6ce78c9d9cadaf0118c6a1677950f06
D1plo1d <d1plo1d@d1plo1d.com>
2012-06-11 20:51:49 -0400

    Adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImplicitCadOrg::Application.config.secret_token = 'f905ec9a0595b3b3640f1afc0a28bea0334dcc70e3f5892553ec58bea23f0b161ffd7ac0d6af15984c46fd9fcfd196a88f47af37e48f1cd7608cabfeb8ab3d33'
