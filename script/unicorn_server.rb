31a4a7d4ecec5e4f8b2f598b4f040407684793c6
D1plo1d <d1plo1d@d1plo1d.com>
2012-06-11 21:46:35 -0400

    Rails Template Update: Improving the unicorn_server.rb script

--- script/unicorn_server.rb
+++ script/unicorn_server.rb
@@ -1,6 +1,17 @@
+env = ENV['RAILS_ENV'] || "development"
+capitalized_env = "#{env[0].upcase}#{env[1..-1]}"
 Dir.chdir(File.join File.dirname(__FILE__), "..")
+
+puts "\n#{capitalized_env} Rails Server: Running Setup\n#{"-"*50}"
+
 `rm ./public/index.html`
-puts "Precompiling.."
-`bundle exec rake assets:precompile`
-puts "[ DONE ]"
-`unicorn -c ./config/unicorn.rb`
+
+if env == "production"
+  puts "Precompiling.."
+  `bundle exec rake assets:precompile`
+  puts "[ DONE ]"
+end
+
+puts "\n#{capitalized_env} Rails Server: Running\n#{"-"*50}"
+
+exec 'unicorn -c ./config/unicorn.rb'
