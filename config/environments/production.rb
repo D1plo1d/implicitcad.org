5c73685ca4d09b188534aa5a75ea04b9309fca60
D1plo1d <d1plo1d@d1plo1d.com>
2012-07-16 21:06:27 -0400

    unicorn config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,13 +6,13 @@ ImplicitCadOrg::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
